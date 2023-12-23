13c704df068dfcd628621e2d7285d4f1092be0ab
Tilak <basnet.tilak2016@gmail.com>
2023-12-23 21:43:48 +0545

    few js codes

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById("hOne"));
+console.log("a");
+let fName = "bir";
+let lName = "dahal";
+let fullName = fName + " " + lName;
+
+console.log("hi", "there", `${fullName}`);
+console.log(`${fName + lName}`);
+
+let age = 32;
+let age = 45;
